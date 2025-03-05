22c18f1efa420d62f2a56a57e7006ff8184de47a
anwitars <maksa22242@gmail.com>
2025-03-05 21:28:43 +0100

    progress: getting familiar with rusqlite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::sync::LazyLock;
+
+use rusqlite::Connection;
+
+const MIGRATIONS_TABLE_NAME: &str = "__migrations__";
+
+static CREATE_MIGRATIONS_TABLE_SQL: LazyLock<String> = LazyLock::new(|| {
+    format!(
+        "CREATE TABLE IF NOT EXISTS {} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )",
+        MIGRATIONS_TABLE_NAME
+    )
+});
+
+fn create_migration_table(conn: &Connection) {
+    conn.execute(&*CREATE_MIGRATIONS_TABLE_SQL, []).unwrap();
+}
+
+fn table_exists<S: AsRef<str>>(conn: &Connection, table_name: S) -> bool {
+    let table_name = table_name.as_ref();
+
+    let mut stmt = conn
+        .prepare(
+            format!(
+                "SELECT name FROM sqlite_master WHERE type='table' AND name='{}'",
+                table_name
+            )
+            .as_str(),
+        )
+        .unwrap();
+
+    let mut rows = stmt.query([]).unwrap();
+
+    rows.next().unwrap().is_some()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::open_in_memory().unwrap();
+    create_migration_table(&conn);
+
+    let table_exists = table_exists(&conn, MIGRATIONS_TABLE_NAME);
+
+    println!("Table exists: {}", table_exists);
+    assert!(table_exists);
 }
